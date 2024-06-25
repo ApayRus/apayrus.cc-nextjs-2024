eed40a20b35bf132e818bf65dff0180844d61de6
aparus <rustamapaev@gmail.com>
2024-06-25 15:33:07 +0300

    add yandex meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
+import Head from 'next/head'
 
 const roboto = Roboto({ weight: ['300'], subsets: ['cyrillic', 'latin'] })
 
 export const metadata: Metadata = {
 	title: 'ApayRus.cc',
-	description: 'Образовательные, языковые приложения'
+	description: 'языковые приложения'
 }
 
 export default function RootLayout({
@@ -17,6 +18,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='ru'>
+			<Head>
+				<meta name='yandex-verification' content='6b138411f9b0b5ee' />
+			</Head>
+
 			<body className={roboto.className}>{children}</body>
 			<GoogleAnalytics gaId='G-Z4CR0LH6DP' />
 		</html>
